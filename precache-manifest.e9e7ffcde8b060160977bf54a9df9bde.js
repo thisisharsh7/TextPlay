067c1349b04b73a2a4ba754ffda5f3f63857f84a
Harsh Kumar <9u.harsh@gmail.com>
2022-06-23 16:10:01 +0530

    Updates

---
+++ precache-manifest.e9e7ffcde8b060160977bf54a9df9bde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "638ff7f0251f55dbb091",
+    "url": "/TextPlay/static/css/main.aa9bd9c3.chunk.css"
+  },
+  {
+    "revision": "638ff7f0251f55dbb091",
+    "url": "/TextPlay/static/js/main.638ff7f0.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextPlay/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextPlay/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "2d3214e33b8dc40835d3",
+    "url": "/TextPlay/static/js/runtime~main.2d3214e3.js"
+  },
+  {
+    "revision": "94dad04ba78c233c7115bc98841a56e3",
+    "url": "/TextPlay/index.html"
+  }
+];
